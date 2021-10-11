93e6834a157432c9cec8dc6a20288556cd443f24
Jaron <jaron_zijlstra@outlook.com>
2021-10-11 09:31:42 +0200

    feat: added message event

---
+++ src/events/message.ts
@@ -0,0 +1,41 @@
+"use strict";
+
+import {ClientEvent} from "discord-extend";
+import {Message, MessageEmbed, TextChannel} from "discord.js";
+import {Client} from "../Client";
+import {defaultOptions} from "../Options";
+
+module.exports = class MessageEvent extends ClientEvent<"messageCreate"> {
+	public constructor() {
+		super("message", "messageCreate");
+	}
+
+	public async run(message: Message) {
+		if (message.author.bot || message.webhookId || !message.guildId) {
+			return;
+		}
+		const client = message.client as Client,
+			{guildId} = message,
+			spawnChannel = client.databases.spawnChannels.get(guildId);
+		let messageAmount = client.messageAmount.get(guildId) ?? 0;
+		if (++messageAmount === defaultOptions.spawnrate && spawnChannel) {
+			messageAmount = 0;
+			const pokemon = await client.pokedex.pokemon.fetchRandom();
+			client.spawnedPokemon.set(guildId, pokemon);
+			((await message.guild?.channels.fetch(spawnChannel)) as TextChannel).send({
+				embeds: [
+					new MessageEmbed({
+						color: "BLUE",
+						title: "A wild pokémon appeared!",
+						description: `Use \`/catch\` to catch this pokémon`,
+						image: {
+							// @ts-ignore
+							url: pokemon.sprites.other["official-artwork"].frontDefault
+						}
+					})
+				]
+			});
+		}
+		client.messageAmount.set(guildId, messageAmount);
+	}
+};
